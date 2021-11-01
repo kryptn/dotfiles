7bbb3f22a9318c2fb45b52db04ee22ee8d81894d
kryptn <kryptn@gmail.com>
2021-11-01 14:50:10 -0600

    put desktop hostname somewhere else

--- .config/terminal/init.sh
+++ .config/terminal/init.sh
@@ -1,9 +1,7 @@
-
-export HOSTNAME="DESKTOP-F98TO1V"
-
 if command -v /usr/bin/keychain &> /dev/null
 then
     # wsl 2 keychain
+    export HOSTNAME="DESKTOP-F98TO1V"
     /usr/bin/keychain --nogui $HOME/.ssh/github
     source $HOME/.keychain/$HOSTNAME-sh
 fi
