fdc8b46132bc8825636db1a3a8fe1c4e3ea96fb5
kryptn <kryptn@gmail.com>
2023-02-18 00:44:10 -0700

    pyenv install

---
+++ .bin/install/pyenv.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+
+sudo apt install build-essential zlib1g zlib1g-dev libssl-dev libbz2-dev libsqlite3-dev libncurses-dev libreadline-dev libffi-dev liblzma-dev
