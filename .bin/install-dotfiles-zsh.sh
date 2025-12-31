d1b8fbced536d227acdaf1e8c157fc091b5e80e0
kryptn <kryptn@gmail.com>
2025-12-31 00:26:15 -0700

    don't default to zsh

--- .bin/install-dotfiles-zsh.sh
+++ .bin/install-dotfiles-zsh.sh
@@ -4,3 +4,6 @@ git clone --bare https://github.com/kryptn/dotfiles.git "$HOME/.cfg"
 
 git --git-dir="$HOME/.cfg/" --work-tree="$HOME" checkout
 git --git-dir="$HOME/.cfg/" --work-tree="$HOME" config status.showUntrackedFiles no
+
+ln -sf "$HOME/.config/terminal/.zshrc" "$HOME/.zshrc"
+echo "Created symlink: ~/.zshrc -> ~/.config/terminal/.zshrc"
